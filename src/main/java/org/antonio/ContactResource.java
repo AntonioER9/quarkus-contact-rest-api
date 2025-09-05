a704817c9e53f0a5c9fb75af3ba5c6b630ddf39d
Antonio Espinoza <aespinozareyes9@gmail.com>
2025-09-05 11:53:51 -0400

    Add ContactResource with CRUD operations and configuration files

--- src/main/java/org/antonio/ContactResource.java
+++ src/main/java/org/antonio/ContactResource.java
@@ -1,4 +1,25 @@
 package org.antonio;
 
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+import java.util.List;
+
+@Path("/api/contacts")
+@Produces("application/json")
+@Consumes
 public class ContactResource {
+
+    @Inject //Dependency Injection
+    EntityManager entityManager;
+
+    // Implement CRUD operations here (Create, Read, Update, Delete)
+    @GET
+    public List<Contact> getAllContacts() {
+        return entityManager.createQuery("from Contact", Contact.class).getResultList();
+    }
 }
